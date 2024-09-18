45e7684436facdff9342edc480b72b5a335e41c7
JLChnToZ <jeremylam619@gmail.com>
2024-09-18 23:00:55 +0800

    Revert unsuccessful enhancement (Back to drawing board)

--- Packages/idv.jlchntoz.ucsplus/Runtime/UdonChips.cs
+++ Packages/idv.jlchntoz.ucsplus/Runtime/UdonChips.cs
@@ -4,25 +4,16 @@ using JLChnToZ.VRC.Foundation;
 
 namespace UCS {
     [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
-    public partial class UdonChips : UdonSharpEventSender {
+    public partial class UdonChips : UdonSharpBehaviour {
         [Tooltip("現在の所持金（初期所持金）")]
-        [FieldChangeCallback(nameof(Money))]
         public float money = 1000;
 
         public string format = "$ {0:F0}";
-
-        float Money {
-            set {
-                money = value;
-                SendEvent("_OnMoneyChanged");
-            }
-        }
     }
 
 #if UNITY_EDITOR && !COMPILER_UDONSHARP
     public partial class UdonChips : ISingleton<UdonChips> {
         public void Merge(UdonChips[] instances) {
-            MergeTargets(instances);
             transform.SetParent(null, false);
             transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
             gameObject.name = "UdonChips";
